8b44217a4e81c6e11fd1d61b20e1e20d4b5d0b6a
castynet <ringemaina@gmail.com>
2021-09-16 14:05:44 +0300

    data insertion query

---
+++ data.sql
@@ -0,0 +1,13 @@
+INSERT INTO tweets
+    ( tweet, image, tweeted)
+VALUES
+    ("hello world, I'm testing a bot -01", 'image link', 0),
+    ("hello world, I'm testing a bot -02", 'image link', 0),
+    ("hello world, I'm testing a bot -03", 'image link', 0),
+    ("hello world, I'm testing a bot -04", 'image link', 0),
+    ("hello world, I'm testing a bot -05", 'image link', 0),
+    ("hello world, I'm testing a bot -06", 'image link', 0),
+    ("hello world, I'm testing a bot -07", 'image link', 0),
+    ("hello world, I'm testing a bot -08", 'image link', 0),
+    ("hello world, I'm testing a bot -09", 'image link', 0),
+    ("hello world, I'm testing a bot -10", 'image link', 0);
